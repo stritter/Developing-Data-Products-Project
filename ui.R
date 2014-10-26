1bcd316ec0e1e7427c8806453543d67b11a5c2ac
stritter <ritter.edx@gmail.com>
2014-10-26 19:40:27 +0100

    checked for grammar and typos

--- ui.R
+++ ui.R
@@ -29,12 +29,20 @@ shinyUI(
         h4('Empirical coverage based on simulation'),
         verbatimTextOutput("coverage"),
         h3('Documentation'),
-        p('Compare the empirical with theoretical coverage probalility.'),
-        p('The app calculates the empirical coverage of a number of samples (number of simulations). The empirical mean and (1-alpha) % confidence intervall of each sample is calculated. Then the app counts how often the theoretical mean (1/lambda), population mean of the exponential distribution, is in the calculated confidence intervall of each sample. The percentage,i.e. the count that the population mean is covered by the confidence intervall divided by the number of simulations, is the result the app displays in the main panel.'),
-        p('Enter the values for the variables defining the simulation on the sidebar panel.'),
-        p('The seed value makes it possible to reproduce the results.'),
-        p('The main panel shows the values for the variables and the empirical coverage of the simulation after the submit button is pressed.'),
-        p('A simple approximation to the exact interval endpoints of the exponential distribution can be derived using a normal approximation. This approximation is used to compute the values for a (1-alpha)% confidence interval')
+        h4('Simulation of the coverage probability of the exponential distribution'),
+
+		p('Compare the empirical with theoretical coverage probability.'),
+
+		p('The app calculates the empirical coverage of a number of samples (number of simulations). The empirical mean and (1-alpha) % confidence interval of each sample is calculated. Then the app counts how often the theoretical mean (1/lambda), i.e. population mean of the exponential distribution, is in the calculated confidence interval of each sample. The percentage, i.e. the count that the population mean is in the confidence interval divided by the number of simulations, is the result the app displays in the main panel.'),
+
+		p('Enter the values for the variables defining the simulation on the sidebar panel.'),
+
+		p('The seed value makes it possible to reproduce the results.'),
+
+		p('The main panel shows the values for the variables and the empirical coverage of the simulation after pressing the submit button.'),
+
+		p('A simple approximation to the exact interval endpoints of the exponential distribution can be derived using a normal approximation. This approximation is used to compute the values for a (1-alpha)% confidence interval.')
+       
     )
   )
 )
